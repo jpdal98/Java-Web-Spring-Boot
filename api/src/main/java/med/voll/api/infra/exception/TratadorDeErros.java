bc67beabab46c73f1060c8be717dc76a2dbba6f7
joao.pedro <joao.pedro@techsolutio.com>
2024-07-30 14:17:49 -0300

    feat: criando filtro de seguranca para a aplicacao

--- api/src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,11 +1,14 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -16,9 +19,14 @@ public class TratadorDeErros {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400(MethodArgumentNotValidException e){
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e) {
         var erros = e.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao :: new).toList());
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> tratarErroIntegridadeBD (DataIntegrityViolationException ex){
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     private record DadosErroValidacao(String campo, String mensagem){
